3482f1d8b13135f126740a8924ca5b5059f911d7
Ion Zota <ion@localhost.localdomain>
2024-12-30 08:48:21 +0200

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -49,12 +49,12 @@
 -- }
 return {
 {
-  "folke/tokyonight.nvim",
+  "savq/melange-nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-        vim.cmd.colorscheme("tokyonight")
+        vim.cmd.colorscheme("melange")
         end
 }
 }
